191d5b15adbe97bc82d52caf7ab60c6e7b150ff3
Artur Romanov <arthurslife@ymail.com>
2024-01-14 13:01:47 +0000

    add REPL, a command line too which allows printing tokens of the input line

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/K1ngArtes/monkey-lang-interpreter-go/repl"
+	"os"
+	"os/user"
+)
 
+func main() {
+	appUser, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+		appUser.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
